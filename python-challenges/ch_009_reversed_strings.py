e447b3426a6a42f4b7878bbd60350eba987e8cb0
Fernando Fonseca <fernando.aaf@hotmail.com>
2023-06-07 01:15:35 -0300

    Create ch_009_reversed_strings.py

---
+++ python-challenges/ch_009_reversed_strings.py
@@ -0,0 +1,13 @@
+'''
+Complete the solution so that it reverses the string passed into it.
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+'''
+
+
+def solution(string):
+    rev_string = ''.join(reversed(string))
+    return rev_string
+
+
+print(solution('world'))
