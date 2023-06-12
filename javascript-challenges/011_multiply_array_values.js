57ce2b65ba75743befb463666bfe22a3fe8583bd
Fernando Fonseca <fernando.aaf@hotmail.com>
2023-06-12 20:46:17 -0300

    Create 011_multiply_array_values.js

---
+++ javascript-challenges/011_multiply_array_values.js
@@ -0,0 +1,17 @@
+/*
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+*/
+
+function grow(x) {
+  let result = 1;
+  for (let i = 0; i < x.length; i++) {
+    result *= x[i];
+  }
+  return result;
+}
+
+console.log(grow([1, 2, 3]) === 6);
+console.log(grow([4, 1, 1, 1, 4]) === 16);
+console.log(grow([2, 2, 2, 2, 2, 2]) === 64); 
