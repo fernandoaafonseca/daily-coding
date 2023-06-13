703ee04b48c74a2c067c64820d6d43e11592f0fb
Fernando Fonseca <fernando.aaf@hotmail.com>
2023-06-13 19:33:30 -0300

    Update ch_012

--- javascript-challenges/ch_012_remove_exclamation_marks.js
+++ javascript-challenges/ch_012_remove_exclamation_marks.js
@@ -3,7 +3,9 @@ Write function RemoveExclamationMarks which removes all exclamation marks from a
 */
 
 function removeExclamationMarks(s) {
-  return s.replace('!', '')
+  // O modificador "g" é usado para realizar uma substituição global, ou seja, todas as ocorrências do padrão serão substituídas.
+  return s.replace(/!/g, '')
 }
 
 console.log(removeExclamationMarks('Hello world!') === 'Hello world')
+console.log(removeExclamationMarks('Hello world!!!') === 'Hello world')
