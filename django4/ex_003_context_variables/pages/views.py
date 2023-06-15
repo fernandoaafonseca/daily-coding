18df371663fe053b8bc2300d88d3b1e608cf3be7
Fernando Fonseca <fernando.aaf@hotmail.com>
2023-06-14 23:47:07 -0300

    Update ex_003

--- django4/ex_003_context_variables/pages/views.py
+++ django4/ex_003_context_variables/pages/views.py
@@ -7,6 +7,7 @@ def home_view(request):
     my_context = {
         'my_list': ['Item 1', 'Item 2', 'Item 3'],
         'my_dict': {'key_1': 'Value 1',
-                    'key_2': 'Value 2'}
+                    'key_2': 'Value 2'},
+        'section': {'home_page': 'Context variables'},
     }
     return render(request, 'home.html', my_context)
