57e757636656edd907f94bf51ab690608ea9326c
Fernando Fonseca <fernando.aaf@hotmail.com>
2023-06-18 00:34:50 -0300

    Update ch_015

--- javascript-challenges/ch_015_next_perfect_square.js
+++ javascript-challenges/ch_015_next_perfect_square.js
@@ -5,7 +5,8 @@ Complete the findNextSquare method that finds the next integral perfect square a
 
 If the parameter is itself not a perfect square then -1 should be returned. You may assume the parameter is non-negative.
 
-Examples:(Input --> Output)
+Examples:
+(Input --> Output)
 121 --> 144
 625 --> 676
 114 --> -1 since 114 is not a perfect square
