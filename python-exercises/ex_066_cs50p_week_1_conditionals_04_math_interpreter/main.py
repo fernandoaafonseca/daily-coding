066e89d42058cdeba480b68710af7f8be258565e
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-09-16 20:35:05 -0300

    Update main.py

--- python-exercises/ex_066_cs50p_week_1_conditionals_04_math_interpreter/main.py
+++ python-exercises/ex_066_cs50p_week_1_conditionals_04_math_interpreter/main.py
@@ -1,27 +1,21 @@
 '''
-Meal Time
-Suppose that you’re in a country where it’s customary to eat breakfast between 7:00 and 8:00, lunch between 12:00 and 13:00, and dinner between 18:00 and 19:00. Wouldn’t it be nice if you had a program that could tell you what to eat when?
+Math Interpreter
+Python already supports math, whereby you can write code to add, subtract, multiply, or divide values and even variables. But let’s write a program that enables users to do math, even without knowing Python.
 
-In meal.py, implement a program that prompts the user for a time and outputs whether it’s breakfast time, lunch time, or dinner time. If it’s not time for a meal, don’t output anything at all. Assume that the user’s input will be formatted in 24-hour time as #:## or ##:##. And assume that each meal’s time range is inclusive. For instance, whether it’s 7:00, 7:01, 7:59, or 8:00, or anytime in between, it’s time for breakfast.
+In a file called interpreter.py, implement a program that prompts the user for an arithmetic expression and then calculates and outputs the result as a floating-point value formatted to one decimal place. Assume that the user’s input will be formatted as x y z, with one space between x and y and one space between y and z, wherein:
 
-Structure your program per the below, wherein convert is a function (that can be called by main) that converts time, a str in 24-hour format, to the corresponding number of hours as a float. For instance, given a time like "7:30" (i.e., 7 hours and 30 minutes), convert should return 7.5 (i.e., 7.5 hours).
+x is an integer
+y is +, -, *, or /
+z is an integer
+For instance, if the user inputs 1 + 1, your program should output 2.0. Assume that, if y is /, then z will not be 0.
 
-def main():
-    ...
+Note that, just as python itself is an interpreter for Python, so will your interpreter.py be an interpreter for math!
 
-
-def convert(time):
-    ...
-
-
-if __name__ == "__main__":
-    main()
 Hints
-Recall that a str comes with quite a few methods, per docs.python.org/3/library/stdtypes.html#string-methods, including split, which separates a str into a sequence of values, all of which can be assigned to variables at once. For instance, if time is a str like "7:30", then
-hours, minutes = time.split(":")
-will assign "7" to hours and "30" to minutes.
+Recall that a str comes with quite a few methods, per docs.python.org/3/library/stdtypes.html#string-methods, including split, which separates a str into a sequence of values, all of which can be assigned to variables at once. For instance, if expression is a str like 1 + 1, then
 
-Keep in mind that there are 60 minutes in 1 hour.
+x, y, z = expression.split(" ")
+will assign 1 to x, + to y, and 1 to z.
 '''
 
 
