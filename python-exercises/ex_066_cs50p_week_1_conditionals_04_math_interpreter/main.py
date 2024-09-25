a0222aaab013b208b8f08619f4699da322219f4e
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-09-25 19:43:17 -0300

    Update main.py

--- python-exercises/ex_066_cs50p_week_1_conditionals_04_math_interpreter/main.py
+++ python-exercises/ex_066_cs50p_week_1_conditionals_04_math_interpreter/main.py
@@ -20,10 +20,63 @@ will assign 1 to x, + to y, and 1 to z.
 
 
 def main():
-    pass
+    user_expression = get_user_expression()
+    n1, operator, n2 = format_expression(user_expression)
+    result = calculate_operation(n1, operator, n2)
+    print(result)
+
+
+def get_user_expression():
+    user_expression = input('Expression: ')
+
+    return user_expression
+
+
+def format_expression(user_expression):
+    raw_n1, operator, raw_n2 = user_expression.split(' ')
+    n1 = float(raw_n1)
+    n2 = float(raw_n2)
+
+    return n1, operator, n2
+
+
+def calculate_operation(n1, operator, n2):
+    match operator:
+        case '+':
+            result = n1 + n2
+        case '-':
+            result = n1 - n2
+        case '*':
+            result = n1 * n2
+        case '/':
+            result = n1 / n2
+
+    final_result = round(result, 1)
+
+    return final_result
 
 
 main()
 
 
 # Test:
+test_n1, test_operator, test_n2 = format_expression('1 + 1')
+print(calculate_operation(test_n1, test_operator, test_n2) == 2.0)
+
+test_n1, test_operator, test_n2 = format_expression('2 - 3')
+print(calculate_operation(test_n1, test_operator, test_n2) == -1.0)
+
+test_n1, test_operator, test_n2 = format_expression('2 * 2')
+print(calculate_operation(test_n1, test_operator, test_n2) == 4.0)
+
+test_n1, test_operator, test_n2 = format_expression('50 / 2')
+print(calculate_operation(test_n1, test_operator, test_n2) == 25.0)
+
+test_n1, test_operator, test_n2 = format_expression('4 / 3')
+print(calculate_operation(test_n1, test_operator, test_n2) == 1.3)
+
+test_n1, test_operator, test_n2 = format_expression('100 - 1')
+print(calculate_operation(test_n1, test_operator, test_n2) == 99.0)
+
+test_n1, test_operator, test_n2 = format_expression('-1 + 100')
+print(calculate_operation(test_n1, test_operator, test_n2) == 99.0)
