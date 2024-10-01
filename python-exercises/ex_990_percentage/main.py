51be3ba1daa96b1a7c545dbf70db94535ac092e7
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-01 15:26:14 -0300

    Add ex_990 unfinished

---
+++ python-exercises/ex_990_percentage/main.py
@@ -0,0 +1,15 @@
+'''
+Formula to calculate the original price with a discount:
+P_original = P_with_discount / (1 - d / 100)
+Where:
+P_original = original price of the product
+P_with_discount = current price with the discount applied
+d = discount percentage
+
+Formula to calculate the original price with an increase:
+P_original = P_with_increase / (1 + a / 100)
+Where:
+P_original = original price of the product
+P_with_increase = current price with the increase applied
+a = increase percentage
+'''
