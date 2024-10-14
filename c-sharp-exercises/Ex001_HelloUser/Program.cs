44753087e32b036ad92c2619d9db7b55ff7ebb79
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-14 14:00:18 -0300

    Create C# project for Ex001 to run on VSCode

---
+++ c-sharp-exercises/Ex001_HelloUser/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+* - Create a new Console project:
+* dotnet console -o NewProjectFolder
+*
+* - Inside the folder, run the project:
+* dotnet run
+*
+* - To run and debug the project:
+* Ctrl + Shift + P -> .NET: Generate Assets for Build and Debug
+*/
+
+namespace HelloUser;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your name: ");
+        string userName = Console.ReadLine();
+
+        // Remove any leading or trailing spaces
+        userName = userName.Trim();
+
+        Console.WriteLine($"Hello, {userName}!");
+
+        // Wait for a key pressing to close the window
+        Console.WriteLine("Press any key to close the window.");
+        Console.ReadKey();
+    }
+}
