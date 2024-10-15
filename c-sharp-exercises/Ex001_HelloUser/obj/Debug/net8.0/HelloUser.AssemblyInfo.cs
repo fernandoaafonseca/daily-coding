1ed0d7492b4ab5c95a02c27a550a3e0d80fa4ade
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-15 18:31:22 -0300

    Update Ex001

--- c-sharp-exercises/Ex001_HelloUser/obj/Debug/net8.0/HelloUser.AssemblyInfo.cs
+++ c-sharp-exercises/Ex001_HelloUser/obj/Debug/net8.0/HelloUser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloUser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2722a243ac9444ed4bcfff0206045d54499cb112")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+036e46717e3c39667de6e0c4e0fa697f72424be6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloUser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloUser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
