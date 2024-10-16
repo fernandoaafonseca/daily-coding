7503b6134eb95efaf0901ab175755f289ed6848f
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-16 14:59:45 -0300

    Update HelloUser.AssemblyInfo.cs

--- c-sharp-exercises/Ex001_HelloUser/obj/Debug/net8.0/HelloUser.AssemblyInfo.cs
+++ c-sharp-exercises/Ex001_HelloUser/obj/Debug/net8.0/HelloUser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloUser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+036e46717e3c39667de6e0c4e0fa697f72424be6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ed0d7492b4ab5c95a02c27a550a3e0d80fa4ade")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloUser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloUser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
