26a7c9656fc0cc4e380c118cb40431cb41d14f3b
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-22 11:57:09 -0300

    Update Program.cs

--- c-sharp/c-sharp-exercises/Ex002_SumOfTwoInputNums/Program.cs
+++ c-sharp/c-sharp-exercises/Ex002_SumOfTwoInputNums/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("olá");
-
+Console.Write("Enter the first whole number: ");
 int userInputNum1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the second whole number: ");
+int userInputNum2 = int.Parse(Console.ReadLine());
+
+int sum = userInputNum1 + userInputNum2;
+
+Console.WriteLine($"The sum of {userInputNum1} + {userInputNum2} = {sum}.");
