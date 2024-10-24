423537ffdd902eb40fe56907b978276dc1cddf91
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-24 12:17:20 -0300

    Update repo

---
+++ courses/course-01-the-modern-python-3-bootcamp/exercises/ch_001_say_your_name.py
@@ -0,0 +1,10 @@
+'''
+Say Your Name
+
+It's your first Python code exercise! Starting out nice and easy :) 
+ Use print()  to tell us your name!
+'''
+
+
+# print your name on the line below
+print('Fernando')
