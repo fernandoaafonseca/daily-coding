2cfa9ac2e7124877d1bd079b051be2830feb81c5
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-28 00:16:19 -0300

    Add function to check fruits calories, function to print result and some tests

--- courses/course-03-cs50p/problem_15_week_2_loops_05_nutrition_facts.py
+++ courses/course-03-cs50p/problem_15_week_2_loops_05_nutrition_facts.py
@@ -17,27 +17,64 @@ Take care to output the fruit’s calories, not calories from fat!
 '''
 
 
+# List with the names of fruits
+fruits_names: list[str] = [
+    'Apple', 'Avocado', 'Banana', 'Cantaloupe', 'Grapefruit', 'Grapes',
+    'Honeydew Melon', 'Kiwifruit', 'Lemon', 'Lime', 'Nectarine', 'Orange',
+    'Peach', 'Pear', 'Pineapple', 'Plums', 'Strawberries', 'Sweet Cherries',
+    'Tangerine', 'Watermelon'
+]
+
+# List with the calories of each fruit
+fruits_calories: list[int] = [
+    130, 50, 110, 50, 60, 90, 50, 90, 15, 20, 60, 80, 60, 100, 50, 70, 50, 100, 50, 80
+]
+
+
 def main():
-    fruits_calories = create_fuits_calories_dict()
+    dict_fruits_calories = create_fuits_calories_dict(
+        fruits_names, fruits_calories)
+    user_input_fruit = get_user_input_fruit()
+    calories = get_calories_value(user_input_fruit, dict_fruits_calories)
+    result = generate_result(user_input_fruit, calories)
+    print(result)
 
 
-def create_fuits_calories_dict() -> dict:
-    # List with the names of fruits
-    fruits_names: str = [
-        'Apple', 'Avocado', 'Banana', 'Cantaloupe', 'Grapefruit', 'Grapes',
-        'Honeydew Melon', 'Kiwifruit', 'Lemon', 'Lime', 'Nectarine', 'Orange',
-        'Peach', 'Pear', 'Pineapple', 'Plums', 'Strawberries', 'Sweet Cherries',
-        'Tangerine', 'Watermelon'
-    ]
-
-    # List with the calories of each fruit
-    calories: int = [
-        130, 50, 110, 50, 60, 90, 50, 90, 15, 20, 60, 80, 60, 100, 50, 70, 50, 100, 50, 80
-    ]
-
-    # Create a dictionary where the keys are the names of the fruits and the values ​​are their respective calories
-    fruits_calories = dict(zip(fruits_names, calories))
+def create_fuits_calories_dict(fruits_names: list[str], fruits_calories: list[int]) -> dict[str, int]:
+    '''
+    Create a dictionary where the keys are the names of the fruits and the values ​​are their respective calories
+    '''
+    fruits_calories = dict(zip(fruits_names, fruits_calories))
     return fruits_calories
+
+
+def get_user_input_fruit() -> str:
+    return input('Plese enter the name of a fruit: ')
+
+
+def get_calories_value(user_input_fruit: str, dict_fruits_calories: dict[str, int]) -> int | None:
+    '''
+    If the fruit exists in the dict, it returns its calories.
+    '''
+    # Case insensitive dictionary search
+    for key in dict_fruits_calories:
+        if key.lower() == user_input_fruit.lower():
+            return dict_fruits_calories[key]
+    return None
+
+
+def generate_result(user_input_fruit: str, calories: int | None) -> str:
+    '''
+    Genenrates the final text for ptinting the name of the fruit and its calories.
+    Returns an empty string if the dict doesn't contain the fruit.
+    '''
+    if calories != None:
+        item_line = f'Item: {user_input_fruit.title()}'
+        calories_line = f'Calories: {calories}'
+        final_text: list[str] = [item_line, calories_line]
+        return '\n'.join(final_text)
+    else:
+        return ''
 
 
 if __name__ == '__main__':
@@ -45,4 +82,22 @@ if __name__ == '__main__':
 
 
 # Test:
-# print(is_valid('ECTO88') == True)
+dict_fruits_calories = create_fuits_calories_dict(
+    fruits_names, fruits_calories)
+print(get_calories_value('apple', dict_fruits_calories) == 130)
+
+print(get_calories_value('Avocado', dict_fruits_calories) == 50)
+
+print(get_calories_value('chocolate', dict_fruits_calories) == None)
+
+print(get_calories_value('Kiwifruit', dict_fruits_calories) == 90)
+
+print(get_calories_value('pear', dict_fruits_calories) == 100)
+
+print(get_calories_value('Strawberry', dict_fruits_calories) == None)
+
+print(get_calories_value('strawberries', dict_fruits_calories) == 50)
+
+print(get_calories_value('Sweet Cherries', dict_fruits_calories) == 100)
+
+print(get_calories_value('Tomato', dict_fruits_calories) == None)
