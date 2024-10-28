75d5e8a412ca41600ee1cf6a0097f4640636081f
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-27 23:02:05 -0300

    Create problem_15_week_2_loops_05_nutrition_facts.py

---
+++ courses/course-03-cs50p/problem_15_week_2_loops_05_nutrition_facts.py
@@ -0,0 +1,45 @@
+'''
+Nutrition Facts
+
+The U.S. Food & Drug Adminstration (FDA) offers downloadable/printable posters that “show nutrition information for the 20 most frequently consumed raw fruits … in the United States. Retail stores are welcome to download the posters, print, display and/or distribute them to consumers in close proximity to the relevant foods in the stores.”
+
+In a file called nutrition.py, implement a program that prompts consumers users to input a fruit (case-insensitively) and then outputs the number of calories in one portion of that fruit, per the FDA’s poster for fruits, which is also available as text. Capitalization aside, assume that users will input fruits exactly as written in the poster (e.g., strawberries, not strawberry). Ignore any input that isn’t a fruit.
+
+Source:
+https://www.fda.gov/food/nutrition-food-labeling-and-critical-foods/raw-fruits-poster-text-version-accessible-version
+
+Hints
+Rather than use a conditional with 20 Boolean expressions, one for each fruit, better to use a dict to associate a fruit with its calories!
+If k is a str and d is a dict, you can check whether k is a key in d with code like:
+if k in d:
+    ...
+Take care to output the fruit’s calories, not calories from fat!
+'''
+
+
+def main():
+    ...
+
+
+raw_fruits_dict_keys = ['fruit', 'calories']
+
+# List with the names of fruits
+fruit_names = [
+    'Apple', 'Avocado', 'Banana', 'Cantaloupe', 'Grapefruit', 'Grapes',
+    'Honeydew Melon', 'Kiwifruit', 'Lemon', 'Lime', 'Nectarine', 'Orange',
+    'Peach', 'Pear', 'Pineapple', 'Plums', 'Strawberries', 'Sweet Cherries',
+    'Tangerine', 'Watermelon'
+]
+
+# List with the calories of each fruit
+calories = [
+    130, 50, 110, 50, 60, 90, 50, 90, 15, 20, 60, 80, 60, 100, 50, 70, 50, 100, 50, 80
+]
+
+
+if __name__ == '__main__':
+    main()
+
+
+# Test:
+# print(is_valid('ECTO88') == True)
