e97cd0a553aab792fc65f13390c4b25b9b4c9599
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-28 19:40:06 -0300

    Create Program.cs

---
+++ courses/course-05-sololearn-introduction-to-c-sharp/Ex02_ProgramStructure/Ex02_ProgramStructure/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Program Structure
+
+The given program should output:
+C# is awesome!
+Coding is fun
+
+But it seems the namespace is missing.
+
+Task
+Fix the given code to generate the required output.
+*/
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("C# is awesome!");
+        Console.WriteLine("Coding is fun");
+    }
+}
