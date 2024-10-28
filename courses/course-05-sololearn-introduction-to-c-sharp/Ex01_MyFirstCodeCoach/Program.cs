5c577d52804392c7c2178ae5af27752c5a0c8256
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-28 19:34:56 -0300

    Create Ex01

---
+++ courses/course-05-sololearn-introduction-to-c-sharp/Ex01_MyFirstCodeCoach/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Your First C# Program
+Let's code for real!
+
+The given program should output the text: "My first C# Code Coach!".
+But something is wrong.
+
+Task
+Complete the code to generate the required output.
+After fixing the code, tap the RUN button to see the result.
+*/
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("My first C# Code Coach!");
+    }
+}
