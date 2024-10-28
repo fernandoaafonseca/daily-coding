e3b30ee3254fa05eac9c29a346f8cf9caaca099f
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-28 19:44:29 -0300

    Create Ex_03

---
+++ courses/course-05-sololearn-introduction-to-c-sharp/Ex03_Alphabet/Ex03_Alphabet/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Alphabet
+
+The given program declares a variable that holds all the letters of the English alphabet.
+
+Task
+Complete the program to output the value of the variable.
+*/
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+        //your code goes here
+        Console.WriteLine(alphabet);
+    }
+}
