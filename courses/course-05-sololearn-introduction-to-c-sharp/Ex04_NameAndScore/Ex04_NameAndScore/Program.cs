8555eaed0d4b909b260b72376cc9d12787c0e45f
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-28 19:47:34 -0300

    Create Ex_04

---
+++ courses/course-05-sololearn-introduction-to-c-sharp/Ex04_NameAndScore/Ex04_NameAndScore/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Name and Score
+
+The given program declares name and score variables, and assigns them their values.
+Then, it outputs the name and the score values.
+However, the code is missing the required types for the variables.
+
+Task
+Add the required types to make the program work as expected.
+*/
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //fix the code
+        string name = "Tom";
+        int score = 163;
+
+        Console.WriteLine(name);
+        Console.WriteLine(score);
+    }
+}
