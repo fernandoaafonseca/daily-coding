bf057c47da3f3d26a787e6aec59ec1ceb0aa8bbb
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-29 04:14:13 -0300

    Create Ex07

---
+++ courses/course-05-sololearn-introduction-to-c-sharp/Ex07_BankAccountBalance/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Bank Account Balance
+
+You want to withdraw some money from your bank account.
+The given program declares balance and withdraw variables and assigns them values.
+
+Task
+Calculate and output the remaining balance after the withdrawal.
+Use the -= shorthand for easier calculation.
+*/
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int balance = 416500;
+        int withdraw = 160000;
+
+        //your code goes here
+        int newBalance = balance - withdraw;
+        Console.WriteLine(newBalance);
+    }
+}
