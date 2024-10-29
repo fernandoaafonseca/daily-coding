76c628434c5323277cae33890eee1dd9dc7f5689
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-29 04:09:40 -0300

    Create Ex06

---
+++ courses/course-05-sololearn-introduction-to-c-sharp/Ex06_GiftBoxes/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Gift Boxes
+
+You are making gift boxes 🎁. Each box contains 9 items.
+
+Task
+Given 200 items in total, you need to calculate and output how many items will be left over if you create gift boxes from all of the items.
+
+The modulo operator % can be useful in solving this challenge.
+*/
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int items = 200;
+        int itemInBox = 9;
+
+        //your code goes here
+        int remainder = items % itemInBox;
+        Console.WriteLine(remainder);
+    }
+}
