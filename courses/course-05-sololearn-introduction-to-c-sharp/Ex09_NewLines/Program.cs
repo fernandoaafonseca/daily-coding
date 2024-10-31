1047488657c6fdbdef5b367b4caba54db981df01
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-10-31 18:58:46 -0300

    Create Ex09

---
+++ courses/course-05-sololearn-introduction-to-c-sharp/Ex09_NewLines/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Strings
+
+The given code declares a text variable with a value of "ABCD".
+
+Task
+Write a program to output each letter on a separate line, like this 👇:
+A
+B
+C
+D
+
+Use \n between the letters 😉.
+*/
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "ABCD";
+        foreach (char c in text)
+        {
+            Console.WriteLine(c);
+        }
+    }
+}
