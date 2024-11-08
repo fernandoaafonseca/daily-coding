83bbe945c6f917ef314f9d98b9b6a170f2b650ad
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-11-08 13:24:01 -0300

    Create Ex18

---
+++ courses/course-05-sololearn-introduction-to-c-sharp/Ex18_BreakTheLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Break The Loop
+
+The given code declares a loop and calculates the multiplication of all numbers from 1 to 10000.
+
+Task
+Make the necessary changes to break the loop when the result is larger than 10000 and output the result.
+
+Hint
+Use an if statement inside the loop and break the loop based on the given condition.
+*/
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int num = 1;
+
+        for (int i = 1; i <= 100; i++)
+        {
+            //your code goes here
+            if (num > 10000)
+            {
+                break;
+            }
+            num *= i;
+        }
+        Console.WriteLine(num);
+    }
+}
