8c64f748766a30c0251b8fb4735a0e2a2d8e9426
fernandoaafonseca <fernando.aaf@hotmail.com>
2024-11-10 19:09:58 -0300

    Create Ex19

---
+++ courses/course-05-sololearn-introduction-to-c-sharp/Ex19_WelcomeSololearner/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Welcome, Sololearner!
+
+We have a method that outputs Welcome, user! as it is called.
+
+Task
+We want to make it more personalized, so redesign the given method so that it will take the given input as the name of the user and output the welcome message with it.
+
+Input Example⬅️: Tommy
+Output Example➡️: Welcome, Tommy!
+
+Don't forget to call the function in Main.
+*/
+
+using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //call the method
+        Welcome();
+    }
+
+    static void Welcome()
+    {
+        //redesign the method
+        string userName = Console.ReadLine();
+        Console.WriteLine($"Welcome, {userName}!");
+    }
+}
