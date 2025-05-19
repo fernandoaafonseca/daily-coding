43b209b4890005e8a104fd1762fa074f0d6329be
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-05-19 10:29:32 -0300

    Create Ex001

---
+++ courses/course-05-freecodecamp-foundational-c-sharp-with-microsoft-learn/Ex001_WriteYourFirstCSharpCode/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+* - Create a new Console project:
+* dotnet new console -o NewProjectFolder
+*
+* - To run and debug the project:
+* Ctrl + Shift + P -> .NET: Generate Assets for Build and Debug
+*
+* - Compile the code:
+* dotnet build
+*
+* - Compile the code in a new folder:
+* dotnet build -c NewFolderName
+*
+* - Inside the folder, run the project:
+* dotnet run
+*/
+
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code.");
