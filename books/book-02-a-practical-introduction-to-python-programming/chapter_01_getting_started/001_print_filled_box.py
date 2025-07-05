9c7dfa6d5c3117d700d4fd991b2361ed6da5f6bb
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-07-05 15:00:52 -0300

    Create 001_print_filled_box.py

---
+++ books/book-02-a-practical-introduction-to-python-programming/chapter_01_getting_started/001_print_filled_box.py
@@ -0,0 +1,17 @@
+'''
+Print a box like the one below.
+
+*******************
+*******************
+*******************
+*******************
+'''
+
+
+def main() -> None:
+	for _ in range(3):
+		print('*' * 19)
+
+
+if __name__ == '__main__':
+	main()
