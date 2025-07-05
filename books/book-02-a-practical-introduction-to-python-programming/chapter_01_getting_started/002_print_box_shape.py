2d4158ae42f9ad5cde514c82ec634446e02f2ade
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-07-05 15:00:57 -0300

    Create 002_print_box_shape.py

---
+++ books/book-02-a-practical-introduction-to-python-programming/chapter_01_getting_started/002_print_box_shape.py
@@ -0,0 +1,26 @@
+'''
+Print a box like the one below.
+
+*******************
+*				  *
+*				  *
+*******************
+'''
+
+
+def main() -> None:
+	for row in range(4):
+		if row in [0, 3]:
+			print('*' *19)
+		else:
+			for column in range(19):
+				#print(column)
+				if column in [0, 18]:
+					print('*', end='')
+				else:
+					print(' ', end='')
+			print()
+
+
+if __name__ == '__main__':
+	main()
