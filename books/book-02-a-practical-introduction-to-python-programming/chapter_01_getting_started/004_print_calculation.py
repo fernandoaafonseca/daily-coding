30f15c65f84ceb75d612981c6be306e7b3b596f6
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-07-05 15:01:07 -0300

    Create 004_print_calculation.py

---
+++ books/book-02-a-practical-introduction-to-python-programming/chapter_01_getting_started/004_print_calculation.py
@@ -0,0 +1,22 @@
+'''
+Write a program that computes and prints the result of
+
+ 512 − 282
+-----------
+47 · 48 + 5.
+
+It is roughly .1017.
+'''
+
+
+def main() -> None:
+	expected_result = 0.1017
+	calc_result = (512 -282) / (47 * 48 + 5)
+	rounded_calc_result = round(calc_result, 4)
+
+	print(calc_result)
+	print(rounded_calc_result == expected_result)
+
+
+if __name__ == '__main__':
+	main()
