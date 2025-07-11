82707f0b79300108990003b8b4c3b55ec1687161
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-07-11 12:23:49 -0300

    Create 014_print_range_with_steps.py

---
+++ books/book-01-a-practical-introduction-to-python-programming/chapter_02_for_loops/014_print_range_with_steps.py
@@ -0,0 +1,12 @@
+'''
+Write a program that uses a for loop to print the numbers 8, 11, 14, 17, 20, . . . , 83, 86, 89.
+'''
+
+
+def main() -> None:
+	for i in range(8, 90, 3):
+		print(i)
+
+
+if __name__ == '__main__':
+	main()
