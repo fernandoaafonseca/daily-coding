458e334af7b59193bbfb92f250f7aa3eedcf45f5
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-07-13 18:30:43 -0300

    Update 023_print_diamond.py

--- books/book-01-a-practical-introduction-to-python-programming/chapter_02_for_loops/023_print_diamond.py
+++ books/book-01-a-practical-introduction-to-python-programming/chapter_02_for_loops/023_print_diamond.py
@@ -15,14 +15,33 @@ diamond should be.
 def main() -> None:
 	diamond_height = int(input('Enter how high the diamond should be: '))
 	print()
-	
-	print_triangle_upside_down(triangle_height)
+
+	print_diamond(diamond_height)
 
 
 def print_diamond(diamond_height: int) -> None:
-	for i in range(triangle_height, 0, -1):
-		size = i
-		print('*' * size)
+	# 5 de altura, 3 é a linha do meio
+	middle_line = (diamond_height + 1) // 2
+
+	# Top lines
+	for amount in range(1, middle_line + 1):
+		# Print empty spaces to the left
+		for _ in range(middle_line - amount):
+			print(' ', end = '')
+		# Print asterisks
+		for _ in range((amount * 2) - 1):
+			print('*', end = '')
+		print()
+
+	# Bottom lines
+	for amount in range(middle_line + 1, diamond_height + 1):
+		# Print empty spaces to the left
+		for _ in range(amount - middle_line):
+			print(' ', end = '')
+		# Print asterisks
+		for _ in range((diamond_height + 1 - amount)*2 - 1):
+			print('*', end = '')
+		print()
 
 
 if __name__ == '__main__':
