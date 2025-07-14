f5ec4ab64522aaf12c5d83c9f69e4e2892ae9e6e
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-07-14 00:53:23 -0300

    Update 023_print_diamond.py

--- books/book-01-a-practical-introduction-to-python-programming/chapter_02_for_loops/023_print_diamond.py
+++ books/book-01-a-practical-introduction-to-python-programming/chapter_02_for_loops/023_print_diamond.py
@@ -20,12 +20,20 @@ def main() -> None:
 
 
 def print_diamond(diamond_height: int) -> None:
+	if diamond_height % 2 == 0:
+		is_height_even = True
+	else:
+		is_height_even = False
+
 	# 5 de altura, 3 é a linha do meio
-	middle_line = (diamond_height + 1) // 2
+	if is_height_even:
+		middle_line = int((diamond_height + 1) / 2)
+	else:
+		middle_line = int((diamond_height + 1) // 2)
 
 	# Top lines
 	for amount in range(1, middle_line + 1):
-		# Print empty spaces to the left
+		# Print empty spaces to the left	
 		for _ in range(middle_line - amount):
 			print(' ', end = '')
 		# Print asterisks
@@ -34,14 +42,25 @@ def print_diamond(diamond_height: int) -> None:
 		print()
 
 	# Bottom lines
-	for amount in range(middle_line + 1, diamond_height + 1):
-		# Print empty spaces to the left
-		for _ in range(amount - middle_line):
-			print(' ', end = '')
-		# Print asterisks
-		for _ in range((diamond_height + 1 - amount)*2 - 1):
-			print('*', end = '')
-		print()
+	if not is_height_even:
+		for amount in range(middle_line + 1, diamond_height + 1):
+			# Print empty spaces to the left
+			for _ in range(amount - middle_line):
+				print(' ', end = '')
+			# Print asterisks
+			for _ in range((diamond_height + 1 - amount) * 2 - 1):
+				print('*', end = '')
+			print()
+
+	else:
+		for amount in range(middle_line, diamond_height):
+			# Print empty spaces to the left
+			for _ in range(amount - middle_line):
+				print(' ', end = '')
+			# Print asterisks
+			for _ in range((diamond_height - amount) * 2 - 1):
+				print('*', end = '')
+			print()
 
 
 if __name__ == '__main__':
