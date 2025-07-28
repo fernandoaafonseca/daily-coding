72339af1524bf678b5dbb732f9dbdd72ee5d743a
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-07-28 17:18:42 -0300

    Create 032_convert_secs_to_mins_and_secs.py

---
+++ books/book-01-a-practical-introduction-to-python-programming/chapter_03_numbers/032_convert_secs_to_mins_and_secs.py
@@ -0,0 +1,13 @@
+'''
+Write a program that asks the user for a number of seconds and prints out how many minutes and seconds that is. For instance, 200 seconds is 3 minutes and 20 seconds.
+
+[Hint: Use the // operator to get minutes and the % operator to get seconds.]
+'''
+
+
+def main() -> None:
+	pass
+
+
+if __name__ == '__main__':
+	main()
