9f97b5d9754a6a96871b8e6f751be294362f006a
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-09-08 16:22:59 -0300

    Update 032_convert_secs_to_mins_and_secs.py

--- books/book-01-a-practical-introduction-to-python-programming/chapter_03_numbers/032_convert_secs_to_mins_and_secs.py
+++ books/book-01-a-practical-introduction-to-python-programming/chapter_03_numbers/032_convert_secs_to_mins_and_secs.py
@@ -6,7 +6,38 @@ Write a program that asks the user for a number of seconds and prints out how ma
 
 
 def main() -> None:
+	secs = get_secs()
+	total_mins, remaining_secs = convert_secs_to_mins(secs)
+	display_result(total_mins, remaining_secs)
 	pass
+
+
+def get_secs() -> int:
+	while True:
+		try:
+			secs = int(input('Enter the number of seconds: '))
+			break
+		except:
+			print('Please enter an integer number.')
+			print()
+
+	return secs
+
+
+def convert_secs_to_mins(secs: int) -> tuple[int, int]:
+	total_mins = secs // 60
+	remaining_secs = secs % 60
+
+	return total_mins, remaining_secs
+
+
+def display_result(total_mins, remaining_secs) -> None:
+	print()
+
+	if remaining_secs > 0:
+		print(f'{total_mins}m, {remaining_secs}s')
+	else:
+		print(f'{total_mins}m')
 
 
 if __name__ == '__main__':
