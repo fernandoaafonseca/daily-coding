c391502b2b20273296488180d7b4ec4e5eeb433b
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-10-13 21:21:57 -0300

    Create 005_lesson_1_review.lua

---
+++ courses/course-12-codecamp-lua/005_lesson_1_review.lua
@@ -0,0 +1,14 @@
+--[[
+Before we move on, let’s write a letter to your future self.
+
+In letter.lua, let’s add the following:
+Goal(s) for yourself.
+Name and date.
+
+Click Run to mail the letter! 📬
+]]
+
+
+print("Dear Self,")
+print('these are the goals for myself.')
+print('My name is Name and the date is Date.')
