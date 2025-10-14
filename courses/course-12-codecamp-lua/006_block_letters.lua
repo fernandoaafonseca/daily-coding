014c92506003e9281b9527ab6ab84636b0ccc8f0
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-10-13 21:22:00 -0300

    Create 006_block_letters.lua

---
+++ courses/course-12-codecamp-lua/006_block_letters.lua
@@ -0,0 +1,23 @@
+--[[
+Write a Lua program called initials.lua that displays the initials of your name in block letters as shown:
+FFFFF	EEEEE	RRRR
+F		E		R   R
+F		E		R   R
+FFF		EEE		RRRR
+F		E		R R
+F		E		R  R
+F		EEEEE	R   R
+
+Happy coding!
+]]
+
+
+-- Fun fact!
+
+print('FFFFF', 'EEEEE', 'RRRR')
+print('F', 'E', 'R   R')
+print('F', 'E', 'R   R')
+print('FFF', 'EEE', 'RRRR')
+print('F', 'E', 'R R')
+print('F', 'E', 'R  R')
+print('F', 'EEEEE', 'R   R')
