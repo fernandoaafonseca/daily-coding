fb12d751e8b426602c286b70554d507bf13de415
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-10-13 21:21:42 -0300

    Create 002_print_multiple_lines.lua

---
+++ courses/course-12-codecamp-lua/002_print_multiple_lines.lua
@@ -0,0 +1,17 @@
+-[[
+Instead of displaying those two lines in the code editor, edit the code so that we output the following pattern in the terminal:
+
+       1
+     2 3
+   4 5 6
+7 8 9 10
+
+Exactly how it is.
+Remember you can use the hint if you need help.
+]]
+
+
+print('       1')
+print('     2 3')
+print('   4 5 6')
+print('7 8 9 10')
