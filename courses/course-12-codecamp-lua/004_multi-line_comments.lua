524b52828e09519daa17dab58bcef10c961e0dc1
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-10-13 21:21:50 -0300

    Create 004_multi-line_comments.lua

---
+++ courses/course-12-codecamp-lua/004_multi-line_comments.lua
@@ -0,0 +1,19 @@
+--[[
+Let’s practice commenting out entire sections of code. Currently, the program is set to self destruct in 10 seconds! (Not really)
+
+Using a multiline comment, comment out the countdown such that we only see "Starting the program" in the console.
+]]
+
+
+print("Starting the program")
+
+--[[print("Self destruction in 10...")
+print("9...")
+print("8...")
+print("7...")
+print("6...")
+print("5...")
+print("4...")
+print("3...")
+print("2...")
+print("1...")]]
