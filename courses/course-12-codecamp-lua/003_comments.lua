b8eade997c0a52e1b771c9a634a66c7d29db3888
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-10-13 21:21:45 -0300

    Create 003_comments.lua

---
+++ courses/course-12-codecamp-lua/003_comments.lua
@@ -0,0 +1,14 @@
+--[[
+Let’s practice adding a comment.
+
+Create a single line comment with -- followed by your favorite emoji.
+
+And then type the following:
+print("Codecademy")
+
+What do you think this program will output?
+]]
+
+
+-- :)
+print("Codecademy")
