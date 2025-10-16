3356fb7a35cd93d002734a26d859b69af49473c8
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-10-16 15:41:59 -0300

    Fix error changing A register to X

--- courses/course-13-nes-programming-with-6502-assembly/ex_02_warmup_01/warmup_01.asm
+++ courses/course-13-nes-programming-with-6502-assembly/ex_02_warmup_01/warmup_01.asm
@@ -1,3 +1,9 @@
+; **************************************************************************
+; Exercise 1:
+; Your goal here is simply load the processor registers A, X and Y with
+; some values.
+; **************************************************************************
+
 .segment "HEADER"
 .org $7FF0
 .byte $4E,$45,$53,$1A,$02,$01,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
@@ -7,7 +13,7 @@
 
 Reset:			; TO-DO:
 	lda #$82	; Load the A register with the literal hexadecimal value $82
-	lda #82		; Load the A register with the literal decimal value 82
+	ldx #82		; Load the X register with the literal decimal value 82
 	ldy $82		; Load the Y register with the value that is inside memory position $82
 
 NMI:
