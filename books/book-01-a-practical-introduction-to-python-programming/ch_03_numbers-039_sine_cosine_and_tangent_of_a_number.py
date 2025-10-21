bc2e910587253793a6b2c67bb1f5596f682b7558
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-10-21 19:39:34 -0300

    Create ch_03_numbers-039_sine_cosine_and_tangent_of_a_number.py

---
+++ books/book-01-a-practical-introduction-to-python-programming/ch_03_numbers-039_sine_cosine_and_tangent_of_a_number.py
@@ -0,0 +1,49 @@
+'''
+Write a program that asks the user for a number and then prints out the sine, cosine, and tangent of that number.
+'''
+
+
+import math
+
+
+def main() -> None:
+	angle_in_degrees = get_angle_in_degrees()
+	angle_in_radians = convert_degrees_to_radians(angle_in_degrees)
+	sine, cosine, tangent = calculate_sin_cos_tan(angle_in_radians)
+	display_result(angle_in_degrees, angle_in_radians, sine, cosine, tangent)
+
+
+def get_angle_in_degrees() -> float:
+	while True:
+		try:
+			angle_in_degrees = float(input('Enter an angle in degrees (°): '))
+			return angle_in_degrees
+		except ValueError:
+			print('Please enter a valid number.\n')
+
+
+def convert_degrees_to_radians(degrees: float) -> float:
+	return math.radians(degrees)
+
+
+def calculate_sin_cos_tan(angle_in_radians: float) -> tuple[float, float, float | str]:
+	sine = math.sin(angle_in_radians)
+	cosine = math.cos(angle_in_radians)
+
+	try:
+		tangent = sine / cosine
+	except ZeroDivisionError:
+		tangent = 'undefined'
+
+	return sine, cosine, tangent
+
+
+def display_result(angle_in_degrees: float, angle_in_radians: float, sine: float, cosine: float, tangent: float) -> None:
+	print(f'The angle {angle_in_degrees:.2f}° is {angle_in_radians:.4f} rad.')
+	print(f'Sine: {sine:.6f}')
+	print(f'Cosine: {cosine:.6f}')
+	print(f'Tangent: {tangent:.6f}')
+
+
+if __name__ == '__main__':
+	main()
