7994f8001e83454b297727ce41d85b96c1a69199
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-10-20 23:30:04 -0300

    Create 038_factorial_of_a_number.py

---
+++ books/book-01-a-practical-introduction-to-python-programming/chapter_03_numbers/038_factorial_of_a_number.py
@@ -0,0 +1,42 @@
+'''
+Write a program that asks the user for a number and prints out the factorial of that number.
+'''
+
+
+def main() -> None:
+	num = get_user_num()
+	factorial = calculate_factorial(num)
+	display_result(num, factorial)
+
+
+def get_user_num() -> int:
+	while True:
+		try:
+			num = int(input('Enter a positive integer number: '))
+			if num >= 0:
+				return num
+			else:
+				raise ValueError()
+		except ValueError:
+			print('Please enter a integer number.\n')
+
+
+def calculate_factorial(num: int) -> int:
+	if num > 0:
+		factorial = num
+
+		for i in range(num - 1, 1, -1):
+			factorial *= i
+
+	else:
+		factorial = 0
+
+	return factorial
+
+
+def display_result(num: int, factorial: int) -> None:
+	print(f'{num}! = {factorial}')
+
+
+if __name__ == '__main__':
+	main()
