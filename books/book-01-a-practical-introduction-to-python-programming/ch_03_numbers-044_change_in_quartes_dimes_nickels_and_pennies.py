399e3596b433ef7f79fb71e56e6b0b9213bc3168
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-10-26 23:13:55 -0300

    Finish the exercise

--- books/book-01-a-practical-introduction-to-python-programming/ch_03_numbers-044_change_in_quartes_dimes_nickels_and_pennies.py
+++ books/book-01-a-practical-introduction-to-python-programming/ch_03_numbers-044_change_in_quartes_dimes_nickels_and_pennies.py
@@ -7,8 +7,8 @@ Write a program that given an amount of change less than $1.00 will print out ex
 
 def main():
 	amount_of_change = get_amount_of_change()
-
-	display_result()
+	quarters, dimes, nickels, pennies = calculate_change(amount_of_change)
+	display_result(amount_of_change, quarters, dimes, nickels, pennies)
 
 
 def get_amount_of_change() -> float:
@@ -23,11 +23,35 @@ def get_amount_of_change() -> float:
 			print('Please enter a valid number.\n')
 
 
-def calculate_change(amount_of_change: float) -> None:
+def calculate_change(amount_of_change: float) -> tuple[int, int, int, int]:
+	quarters, dimes, nickels, pennies = 0, 0, 0, 0
+
+	remaining_change = amount_of_change
+	while remaining_change >= 0.25:
+		quarters += 1
+		remaining_change -= 0.25
+
+	while remaining_change >= 0.10:
+		dimes += 1
+		remaining_change -= 0.10
+
+	while remaining_change >= 0.05:
+		nickels += 1
+		remaining_change -= 0.05
+
+	while remaining_change >= 0.01:
+		pennies += 1
+		remaining_change -= 0.01
+
+	return quarters, dimes, nickels, pennies
 
 
-def display_result() -> None:
-	print(f'')
+def display_result(amount_of_change: float, quarters: int, dimes: int, nickels: int, pennies: int) -> None:
+	print(f'Change: ${amount_of_change}')
+	print(f'Quarters ($0.25): {quarters}')
+	print(f'Dimes ($0.10): {dimes}')
+	print(f'Nickels ($0.05): {nickels}')
+	print(f'Pennies ($0.01): {quarters}')
 
 
 if __name__ == '__main__':
