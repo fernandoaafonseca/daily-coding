091989449a90fda7c376973c1eb554bc33dc1abd
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-06 19:09:42 -0300

    Create ch_06_strings-088_capitalize_first_letter_each_word.py

---
+++ books/book-01-a-practical-introduction-to-python-programming/ch_06_strings-088_capitalize_first_letter_each_word.py
@@ -0,0 +1,27 @@
+'''
+Write a program that asks the user to enter their name in lowercase and then capitalizes the first letter of each word of their name.
+'''
+
+def main() -> None:
+	user_name = get_user_name()
+	user_name_capitalized = user_name.title()
+	display_result(user_name, user_name_capitalized)
+
+
+def get_user_name() -> str:
+	while True:
+		try:
+			user_name = str(input(f'Enter your name: '))
+			return user_name
+		except ValueError:
+			print('\nPlease enter a valid string.\n')
+
+
+def display_result(user_name:str, user_name_capitalized: str) -> None:
+	print()
+	print(f'You entered your name as follows: {user_name}.')
+	print(f'Capitalizing your name: {user_name_capitalized}.')
+
+
+if __name__ == '__main__':
+	main()
