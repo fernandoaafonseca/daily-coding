06231a9cecb06e720a9dadac58a8f81397324d91
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-08 18:33:27 -0300

    Just fix type hint of the variable "alphabet"

--- books/book-01-a-practical-introduction-to-python-programming/ch_06_strings-091_block_of_letters_changing_alphabetical_order.py
+++ books/book-01-a-practical-introduction-to-python-programming/ch_06_strings-091_block_of_letters_changing_alphabetical_order.py
@@ -18,7 +18,7 @@ def main() -> None:
 	display_result(alphabet_doubled)
 
 
-def display_result(alphabet_doubled: list[str, ...]) -> None:
+def display_result(alphabet_doubled: str) -> None:
 	num_lines = 26
 	line_length = 26
 	starting_char = 0
