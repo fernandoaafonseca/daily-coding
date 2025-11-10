3f827680f4489843cb3dd5c1df8460dcf099f066
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-10 07:15:58 -0300

    Create ch_06_strings-102_encrypt_rail_fence_cipher_n_rails.py

---
+++ books/book-01-a-practical-introduction-to-python-programming/ch_06_strings-102_encrypt_rail_fence_cipher_n_rails.py
@@ -0,0 +1,58 @@
+'''
+A more general version of the above technique is the "rail fence cipher", where instead of breaking things into evens and odds, they are broken up by threes, fours or something larger. For instance, in the case of threes, the string "secret message" would be broken into three groups. The first group is "sr sg", the characters at indices 0, 3, 6, 9 and 12. The second group is "eemse", the characters at indices 1, 4, 7, 10, and 13. The last group is "ctea", the characters at indices 2, 5, 8, and 11. The encrypted message is "sr sgeemsectea".
+
+    (a) Write a program the asks the user for a string and uses the rail fence cipher in the threes case to encrypt the string.
+    (b) Write a decryption program for the threes case.
+    (c) Write a program that asks the user for a string, and an integer determining whether to break things up by threes, fours, or whatever. Encrypt the string using the rail-fence cipher.
+    (d) Write a decryption program for the general case.
+'''
+
+
+def main() -> None:
+    user_string = get_user_string()
+    num_rails = get_num_rails(user_string)
+    encrypted_str = encrypt_rail_fence_three(user_string, num_rails)
+    display_result(encrypted_str)
+
+
+def get_user_string() -> str:
+    while True:
+        try:
+            user_string = str(input('Enter a message to be encrypted: '))
+            if len(user_string.replace(' ', '')) > 0:
+                return user_string
+            else:
+                raise ValueError
+        except ValueError:
+            print('\nPlease enter a non-empty string.\n')
+
+
+def get_num_rails(user_string: str) -> int:
+    while True:
+        try:
+            num_rails = int(input('Enter the number of rails: '))
+            if len(user_string) > num_rails:
+                return num_rails
+            else:
+                raise ValueError
+        except ValueError:
+            print('\nPlease enter a value smaller than the length of the message.\n')
+
+
+def encrypt_rail_fence_three(user_string: str, num_rails: int) -> str:
+    encrypted_str = ''
+
+    for i in range(num_rails):
+        encrypted_str += user_string[i::num_rails]
+
+    return encrypted_str
+
+
+def display_result(encrypted_str: str) -> None:
+    print()
+    print('Encrypted message:')
+    print(encrypted_str)
+
+
+if __name__ == "__main__":
+    main()
