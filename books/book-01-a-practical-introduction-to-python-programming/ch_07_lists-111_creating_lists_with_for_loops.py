1fe35273156c1677cc5a0bd39d3c5c283488cc61
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-12 09:04:26 -0300

    Create ch_07_lists-111_creating_lists_with_for_loops.py

---
+++ books/book-01-a-practical-introduction-to-python-programming/ch_07_lists-111_creating_lists_with_for_loops.py
@@ -0,0 +1,47 @@
+'''
+Create the following lists using a for loop.
+
+	(a) A list consisting of the integers 0 through 49
+	(b) A list containing the squares of the integers 1 through 50.
+	(c) The list ['a', 'bb', 'ccc', 'dddd', ...] that ends with 26 copies of the letter z.
+'''
+
+
+import string
+
+
+ALPHABET = string.ascii_lowercase
+
+
+def main():
+	list_of_ints = generate_list_of_ints()
+	list_of_squares = generate_list_of_squares()
+	list_of_letters = generate_list_of_letters(ALPHABET)
+	display_result(list_of_ints, list_of_squares, list_of_letters)
+
+
+def generate_list_of_ints() -> list[int]:
+	return [num for num in range(0, 50)]
+
+
+def generate_list_of_squares() -> list[int]:
+	return [num**2 for num in range(1, 51)]
+
+
+def generate_list_of_letters(ALPHABET) -> list[str]:
+	list_of_letters = []
+
+	for index, letter in enumerate(ALPHABET):
+		list_of_letters.append(letter * (index + 1))
+
+	return list_of_letters
+
+
+def display_result(list_of_ints: list[int], list_of_squares: list[int], list_of_letters: list[str]) -> None:
+	print(f'List of integers: \n{list_of_ints}')
+	print(f'\nList of squares: \n{list_of_squares}')
+	print(f'\nList of letters: \n{list_of_letters}')
+
+
+if __name__ == '__main__':
+	main()
