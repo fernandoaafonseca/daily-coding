3b3f9c9e20063592bf3bebc47aa21e66754268ce
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:46:32 -0300

    Create sample_36_exceptions_valueerror.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_36_exceptions_valueerror.py
@@ -0,0 +1,10 @@
+'''
+The ValueError exception is raised when a function receives a value of the correct type, but the value itself is inappropriate or unacceptable.
+
+For example, the int() function can be called on strings, but only when all characters in the string are numerical values.
+'''
+
+
+data = '1109A'
+
+num_data = int(data)
