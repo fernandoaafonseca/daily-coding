cd44d52c71e0cad8f963c7e149c24af833d91a6c
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:45:24 -0300

    Create sample_14_dictionaries_access_values_using_keys.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_14_dictionaries_access_values_using_keys.py
@@ -0,0 +1,16 @@
+'''
+Values in lists and tuples are accessed using indexes.
+
+To access values in dictionaries, you need to use the keys.
+'''
+
+
+car = {
+  'brand': 'Audi',
+  'model': 'Q5',
+  'year': '2008'
+}
+
+print(car['brand'])
+print(car['model'])
+print(car['year'])
