11800996768a99c38402a5ce574719fe5e1b88bf
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:45:06 -0300

    Create sample_09_sets_clear_function.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_09_sets_clear_function.py
@@ -0,0 +1,9 @@
+'''
+The clear() function doesn't accept an argument and removes all the items from a set.
+'''
+
+
+students = {'Anna', 'Mery', 'Mery', 'Jonathan'}
+students.clear()
+
+print(students)
