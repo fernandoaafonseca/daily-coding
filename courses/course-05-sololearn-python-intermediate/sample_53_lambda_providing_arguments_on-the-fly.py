7a3e01296244e4a98b1b63c6eb9a172d574d943d
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:47:29 -0300

    Create sample_53_lambda_providing_arguments_on-the-fly.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_53_lambda_providing_arguments_on-the-fly.py
@@ -0,0 +1,8 @@
+'''
+You can provide arguments to lambda expressions on-the-fly by adding them in parentheses immediately after the lambda function. The lambda expression should be also enclosed in parentheses.
+'''
+
+
+res = (lambda x, y: x + y) (2, 3)
+
+print(res)
