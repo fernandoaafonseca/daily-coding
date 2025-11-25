d9e71bd0560d9ca1e3a230de0a6dae49db6926d4
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:45:09 -0300

    Create sample_10_sets_union_function.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_10_sets_union_function.py
@@ -0,0 +1,11 @@
+'''
+Calling the union() function returns a new set with all elements from both sets, omitting duplicates.
+'''
+
+
+set1 = {'apple', 'banana'}
+set2 = {'banana', 'cherry'}
+
+combined_set = set1.union(set2)
+
+print(combined_set)
