7e242c5e00798ad98682b163895d857944c3337b
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:45:30 -0300

    Create sample_16_dictionaries_values_and_keys_functions.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_16_dictionaries_values_and_keys_functions.py
@@ -0,0 +1,15 @@
+'''
+You can get all the values and keys of a dictionary using the values() and keys() functions, respectively.
+'''
+
+
+contact = {
+  'name': 'John',
+  'company': 'Google',
+}
+
+info_keys = contact.keys()
+info_values = contact.values()
+
+print(info_keys)
+print(info_values)
