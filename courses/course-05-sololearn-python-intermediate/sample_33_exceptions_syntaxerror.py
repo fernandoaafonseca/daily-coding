6cde464da4a86c2da3bdb5a90f557877f9390f15
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:46:24 -0300

    Create sample_33_exceptions_syntaxerror.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_33_exceptions_syntaxerror.py
@@ -0,0 +1,8 @@
+'''
+The SyntaxError exception is raised when a syntax mistake in the code is encountered. This could be due to various reasons such as missing punctuation (like commas, parentheses or colons).
+'''
+
+
+score = 85
+if score >= 80 # Missing colon
+  print('Passed')
