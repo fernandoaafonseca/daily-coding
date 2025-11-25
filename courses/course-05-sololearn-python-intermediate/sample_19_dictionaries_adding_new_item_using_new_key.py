855c85fc9c74e0ec52502739b7ad2bef9cb96232
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:45:39 -0300

    Create sample_19_dictionaries_adding_new_item_using_new_key.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_19_dictionaries_adding_new_item_using_new_key.py
@@ -0,0 +1,14 @@
+'''
+You can add a new item by providing a new key and assigning a value to it.
+'''
+
+
+user = {
+  'Name': 'Albert',
+  'Age': 29
+}
+
+user['Grade'] = 7
+
+print(user['Grade'])
+print(user.items())
