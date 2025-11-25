5a21749cf8cac23f39a9ce284badcb049887a49f
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:46:30 -0300

    Create sample_35_exceptions_typeerror.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_35_exceptions_typeerror.py
@@ -0,0 +1,8 @@
+'''
+The TypeError exception is raised when a function is called on a value of an inappropriate type. For example, the len() function can be called only on iterables (like strings, lists, etc.).
+'''
+
+
+message = 5
+
+length = len(message)
