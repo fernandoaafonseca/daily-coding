ac59b6f0220382814f0456d3004c9bfa0dad3b42
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:46:01 -0300

    Create sample_26_dictionaries_using_for_loops_to_get_values.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_26_dictionaries_using_for_loops_to_get_values.py
@@ -0,0 +1,13 @@
+'''
+Use the values() function to iterate through the car dictionary's values
+'''
+
+
+car = {
+  'Brand': 'Ford',
+  'Model': 'Mustang',
+  'Color': 'red'
+}
+
+for i in car.values():
+  print(i)
