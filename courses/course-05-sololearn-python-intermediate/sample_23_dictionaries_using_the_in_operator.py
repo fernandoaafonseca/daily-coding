fd7648b62db578a647c55f4b3aa3ab4a3fb71e15
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:45:52 -0300

    Create sample_23_dictionaries_using_the_in_operator.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_23_dictionaries_using_the_in_operator.py
@@ -0,0 +1,12 @@
+'''
+You can use the in operator to check if a key or a value occurs in a dictionary.
+'''
+
+
+car = {
+  'Brand': 'Ford',
+  'Model': 'Mustang',
+  'Color': 'red'
+}
+
+print('Color' in car)
