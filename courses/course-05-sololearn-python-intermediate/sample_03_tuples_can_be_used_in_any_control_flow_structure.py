9238f0716295d4e01e4a7acad91918e06d9c78e1
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:43:34 -0300

    Create sample_03_tuples_can_be_used_in_any_control_flow_structure.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_03_tuples_can_be_used_in_any_control_flow_structure.py
@@ -0,0 +1,12 @@
+'''
+You can use tuples in any control flow structures, such as if-else statements and loops, just as you would with lists.
+
+For example:
+'''
+
+
+points = (12, 14, 9, 10, 9)
+
+for point in points:
+  if point > 10:
+    print(f'{point}: passed')
