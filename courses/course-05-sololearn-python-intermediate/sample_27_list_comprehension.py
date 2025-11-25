a1b1b39566746b506c78fdcf57466ff14956bc22
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:46:04 -0300

    Create sample_27_list_comprehension.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_27_list_comprehension.py
@@ -0,0 +1,8 @@
+'''
+List comprehensions are useful shorthands for such operations. They offer a shorter and more readable way to create lists with various settings using just a single line of code.
+'''
+
+
+nums = [x for x in range(1, 51)]
+
+print(nums)
