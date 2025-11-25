9b394d9855a3579339dafe997261d61a6365f2f9
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:46:27 -0300

    Create sample_34_exceptions_indexerror.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_34_exceptions_indexerror.py
@@ -0,0 +1,8 @@
+'''
+The IndexError is raised when you attempt to access an element of an iterable, ordered collection, such as lists and tuples, using an index that is outside its valid range.
+'''
+
+
+cars = ['BMW', 'Tesla', 'Ford']
+
+print(cars[3])
