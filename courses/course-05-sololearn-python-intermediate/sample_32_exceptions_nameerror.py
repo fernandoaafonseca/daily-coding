06f25b3454764ac8c34af3ce36e02636a4dd34fd
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:46:21 -0300

    Create sample_32_exceptions_nameerror.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_32_exceptions_nameerror.py
@@ -0,0 +1,11 @@
+'''
+There are several types of exceptions in Python.
+
+The NameError exception is raised when an unknown variable is used.
+
+Run the code to see the exception message.
+'''
+
+
+name = 'Anna'
+print(surname)
