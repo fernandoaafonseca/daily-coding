ca732da60cb426af0c2f5d517b29ef5afaeecf42
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:45:55 -0300

    Create sample_24_dictionaries_values_function.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_24_dictionaries_values_function.py
@@ -0,0 +1,12 @@
+'''
+To check if a value occurs in a dictionary, you need to use the values() function.
+'''
+
+
+car = {
+  'Brand': 'Ford',
+  'Model': 'Mustang',
+  'Color': 'red'
+}
+
+print('red' in car.values())
