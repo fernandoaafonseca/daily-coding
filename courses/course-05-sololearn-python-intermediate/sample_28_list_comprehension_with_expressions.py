e160edea28b9b641617f27772afa7f1915a6a63b
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:46:07 -0300

    Create sample_28_list_comprehension_with_expressions.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_28_list_comprehension_with_expressions.py
@@ -0,0 +1,9 @@
+'''
+You can apply any expression to each item in the list being created with a list comprehension.
+
+For instance, the following code doubles each value in range(10) and stores the results in a list:'''
+
+
+nums = [x * 2 for x in range(10)]
+
+print(nums)
