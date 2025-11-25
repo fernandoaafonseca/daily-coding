808bec1c9da39359306a51fd46ecc015642e4e71
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:45:58 -0300

    Create sample_25_dictionaries_using_for_loops_to_get_keys.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_25_dictionaries_using_for_loops_to_get_keys.py
@@ -0,0 +1,15 @@
+'''
+You can iterate through a dictionary using a for loop.
+
+If you loop through a dictionary, it will return the keys.
+'''
+
+
+car = {
+  'Brand': 'Ford',
+  'Model': 'Mustang',
+  'Color': 'red'
+}
+
+for i in car:
+  print(i)
