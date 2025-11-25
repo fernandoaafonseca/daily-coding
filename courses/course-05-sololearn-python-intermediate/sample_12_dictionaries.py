c6c75d4dd19bbaae0d5254d43e326b3f8137f297
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:45:15 -0300

    Create sample_12_dictionaries.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_12_dictionaries.py
@@ -0,0 +1,12 @@
+'''
+Dictionaries are collection types used to store data in key:value pairs, which are considered as items. They are ideal for organizing data into pairs, where each piece of data (value) has its unique identifier (key).
+
+For example:
+'''
+
+
+product = {
+  'name': 'pen',
+  'color': 'red',
+  'price': 79
+}
