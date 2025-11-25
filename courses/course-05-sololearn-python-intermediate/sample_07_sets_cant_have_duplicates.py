687e339bb01b185ae4ff493bcbd669923cedc72d
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:45:00 -0300

    Create sample_07_sets_cant_have_duplicates.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_07_sets_cant_have_duplicates.py
@@ -0,0 +1,10 @@
+'''
+Sets can't have duplicates, which is very helpful when developers need to ensure that each item in a collection is unique. For example, in social media apps, your friends list should not have duplicates.
+
+Adding duplicate items to a set doesn't cause an error; instead, it's ignored.
+'''
+
+
+friends = {'Anna', 'Mery', 'Mery', 'Jonathan'}
+
+print(friends)
