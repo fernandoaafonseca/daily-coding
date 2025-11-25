24ce9e34e6cb8b0bcacc5f98add56d567523545a
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 19:44:15 -0300

    Create sample_04_tuple_unpacking.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_04_tuple_unpacking.py
@@ -0,0 +1,11 @@
+'''
+Tuple unpacking allows for assigning tuple items to variables. The values will be assigned in the order they appear in the tuple.
+'''
+
+
+birthday_date = (12, 'August', 1993)
+day, month, year = birthday_date
+
+print(day)
+print(month)
+print(year)
