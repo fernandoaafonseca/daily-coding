8c3f37f9a36a556fbfb366cec38602bbede34420
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 21:48:42 -0300

    Create sample_74_oop_everything_in_python_is_an_object.py

---
+++ courses/course-05-sololearn-python-intermediate/sample_74_oop_everything_in_python_is_an_object.py
@@ -0,0 +1,24 @@
+'''
+Everything in Python, including functions, is an object. For instance, integers are instances of the int class, and functions are instances of the function class, among others. This object-oriented nature underlies Python's flexibility and power.
+'''
+
+
+# Function
+def greet():
+  print("Welcome!")
+
+
+# List
+prices = [55, 68, 77, 36]
+
+# Data types
+x = 5
+city = "London"
+is_open = True
+
+
+print(type(greet))
+print(type(prices))
+print(type(x))
+print(type(city))
+print(type(is_open))
