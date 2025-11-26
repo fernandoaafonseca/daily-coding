85902a26ec730151e202f77ddf34e2b02e5539a1
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-11-25 22:57:03 -0300

    Update description comment

--- courses/course-05-sololearn-python-intermediate/sample_90_class_and_static_methods_static_methods_decorator.py
+++ courses/course-05-sololearn-python-intermediate/sample_90_class_and_static_methods_static_methods_decorator.py
@@ -2,6 +2,8 @@
 Static methods are similar to class methods, except they don't receive any additional arguments; they are identical to normal functions that belong to a class.
 
 They are marked with the @staticmethod decorator.
+
+When should you use static methods instead of class methods? Static methods don't accept the cls parameter, meaning they can't access or modify the class's state. They are useful when you require functionality that doesn't depend on the class's behavior or instance state and doesn't affect it. Essentially, static methods are suited for tasks that are self-contained and do not require knowledge of the class or instance.
 '''
 
 
