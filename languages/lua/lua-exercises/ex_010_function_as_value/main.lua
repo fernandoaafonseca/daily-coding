368c39cf44346ac98085214dea61e44d4ec0b94c
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-12-03 06:51:32 -0300

    Create main.lua

---
+++ languages/lua/lua-exercises/ex_010_function_as_value/main.lua
@@ -0,0 +1,11 @@
+--[[
+Do the following:
+
+    1. Create a function named 'square' and store it in a variable.
+
+    2. Then assign the same function to a second variable named 'square_copy'.
+
+    3. Call both variables and show that they behave identically.
+
+    4. Add a comment explaining that in Lua, functions are values just like numbers or strings, and can be stored in variables or passed around freely.
+]]
