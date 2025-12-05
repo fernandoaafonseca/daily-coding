8590e637c3dde45d317fdfbe0050bd25f7397046
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-12-05 09:43:14 -0300

    Create main.lua

---
+++ languages/lua/lua-exercises/ex_011_table_basics_creation_and_access/main.lua
@@ -0,0 +1,15 @@
+--[[
+Do the following:
+
+    1. Create a table named 'player' with the fields:
+            name
+            level
+            experience
+
+    2. Print each field using dot-notation:
+            player.name
+
+    3. Add a new field named 'gold' after the table is created.
+
+    4. Print the final table values.
+]]
