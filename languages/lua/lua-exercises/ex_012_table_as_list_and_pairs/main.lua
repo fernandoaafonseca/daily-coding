d33031cc395c2aac9e2a54fc7909b9e682714238
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-12-05 09:43:18 -0300

    Create main.lua

---
+++ languages/lua/lua-exercises/ex_012_table_as_list_and_pairs/main.lua
@@ -0,0 +1,14 @@
+--[[
+Do the following:
+
+    1. Create a table named 'numbers' containing 5 numeric values.
+
+    2. Loop through it using:
+            - numeric for (i = 1, #numbers)
+            - pairs(numbers)
+
+    3. Print both loops and observe the difference.
+
+    4. Add comments explaining why #numbers works only for
+       sequences without holes.
+]]
