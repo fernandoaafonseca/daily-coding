80caf54ce0a497097080bae85cccdecc237f9904
Fernando Fonseca <fernando.aaf@hotmail.com>
2025-12-05 09:43:21 -0300

    Create main.lua

---
+++ languages/lua/lua-exercises/ex_013_map_function/main.lua
@@ -0,0 +1,15 @@
+--[[
+Do the following:
+
+    1. Create a function named 'map' that:
+            - receives a list
+            - receives a function
+            - returns a new list where each element is the
+              result of applying the function to the input list.
+
+    2. Use the 'map' function to:
+            - square numbers
+            - triple numbers
+
+    3. Print the results.
+]]
